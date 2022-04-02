e4480ab373e41faf13d0778a828b05163854ec18
Monika Lenartowicz <nika.nel@interia.pl>
2022-04-02 10:36:31 +0200

    Added calculate currency function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const currencyOne = document.querySelector(".select--one");
+const amountOne = document.querySelector(".appBody__input--one");
+const currencyTwo = document.querySelector(".select--two");
+const amountTwo = document.querySelector(".appBody__input--two");
+const swapButton = document.querySelector(".swap");
+const rateInfo = document.querySelector(".rateInfo");
 
+const calculate = () => {
+	fetch(`https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
+		.then(res => res.json())
+		.then(data => {
+			const currency1 = currencyOne.value;
+			const currency2 = currencyTwo.value;
+			const rate = data.rates[currency2];
+
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`;
+			amountTwo.value = amountOne.value * rate.toFixed(2);
+		});
+};
+
+currencyOne.addEventListener("change", calculate);
+currencyTwo.addEventListener("change", calculate);
